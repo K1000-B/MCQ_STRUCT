74f25a6271dfa741d32eafd98de2d741133a34a3
Camile Briard <briard.camile@gmail.com>
2026-01-19 18:55:44 +0100

    rename app

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const APP_NAME = 'PulsePrep'
+export const APP_NAME = 'POLIMI M1 AEROSPACE STRUCTURES MCQ TRAINER'
 export const DONATION_URL = 'https://example.com/donate'
 export const CONTACT_EMAIL = 'club-drapeaux34@icloud.com'
