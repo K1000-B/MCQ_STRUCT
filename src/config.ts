60bac72756a2e2d3de88af547374785d7ea72cb2
Camile Briard <briard.camile@gmail.com>
2026-01-19 19:43:34 +0100

    donations

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const APP_NAME = 'POLIMI M1 AEROSPACE STRUCTURES MCQ TRAINER'
-export const DONATION_URL = 'https://example.com/donate'
+export const DONATION_URL = 'https://www.paypal.com/donate/?hosted_button_id=G67U45UYYP562'
 export const CONTACT_EMAIL = 'club-drapeaux34@icloud.com'
