a63f60f509e6724f9500b176099827982f34bd16
Camile Briard <briard.camile@gmail.com>
2026-01-22 18:37:22 +0100

    pop up disabled

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const DONATION_URL = 'https://www.paypal.com/donate/?hosted_button_id=G67
 export const CONTACT_EMAIL = 'club-drapeaux34@icloud.com'
 
 export const WHATS_NEW_CONFIG = {
-  enabled: true,
+  enabled: false,
   title: "What's new",
   items: [
     'Mistakes recap at the end of each session',
